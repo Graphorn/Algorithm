e9bf4ec49c54c96c129e485d211e984887a5637a
seektech@126.com <limiao5002361995>
2016-10-11 17:06:30 +0800

    stringmatch

---
+++ src/limiao/stringmatch/AhoCorasick.java
@@ -0,0 +1,18 @@
+package limiao.stringmatch;
+
+/**
+ * AC自动机：Aho-Corasick automation
+ * @author seektech
+ * Oct 10, 2016 9:52:15 PM
+ */
+public class AhoCorasick {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/limiao/stringmatch/Sunday.java
@@ -0,0 +1,18 @@
+package limiao.stringmatch;
+
+/**
+ * Sunday
+ * @author seektech
+ * Oct 10, 2016 9:54:40 PM
+ */
+public class Sunday {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
