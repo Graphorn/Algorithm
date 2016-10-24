58be6afb9558a8522562de0a35bf154cabe0aa5a
seektech@126.com <limiao5002361995>
2016-10-24 19:21:42 +0800

    414 not complete

---
+++ src/limiao/leetcode/No_414_ThirdMaximumNumber.java
@@ -0,0 +1,56 @@
+package limiao.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 414. Third Maximum Number
+ * @author seektech
+ * Oct 24, 2016 5:00:45 PM
+ */
+public class No_414_ThirdMaximumNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int source[] = {2,2,3,1};
+		System.out.println(thirdMax(source));
+	}
+	public static int thirdMax(int[] nums) {
+		if(nums.length == 1) return nums[0];
+		if(nums.length == 2) return nums[0]>=nums[1]?nums[0]:nums[1];
+		
+		List<Integer> list = new ArrayList<>();
+		list.add(nums[0]);
+		for(int i=1; i<nums.length; i++){
+			if(nums[i]<list.get(list.size()-1)){
+				list.add(nums[i]);
+			}
+			if(nums[i]>list.get(list.size()-1)){
+				int temp = list.get(list.size()-1);
+				list.set(list.size()-1,temp);
+				list.add(nums[i]);
+			}
+			if(list.size() == 3) break;
+		}
+		
+		
+		for(int i=0; i<nums.length; i++){
+			if(nums[i]>list.get(2)&&nums[i]<list.get(1)){
+				list.set(2, nums[i]);
+			}
+			if(nums[i]>list.get(1)&&nums[i]<list.get(0)){
+				list.set(2, list.get(1));
+				list.set(1, nums[i]);
+			}
+			if(nums[i]>list.get(0)){
+				list.set(2, list.get(1));
+				list.set(1, list.get(0));
+				list.set(0, nums[i]);
+			}
+		}
+        return nums[2];
+    }
+}
---
+++ src/limiao/other/SearchLostInteger.java
@@ -0,0 +1,54 @@
+package limiao.other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 寻找缺失的整数
+ * @author seektech
+ * Oct 18, 2016 9:40:10 AM
+ */
+public class SearchLostInteger {
+
+	/**一个无序数组里有99个不重复的正整数，范围从1到100，
+	 * 唯独缺少一个数，请找出缺失的整数
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sourceArray[] = {5,2,6,4,3,8,1,7,10};
+	}
+
+	/**
+	 * 方法一：利用所有数的和减去已经知道的数就是缺失的数了
+	 * @param source
+	 * @return
+	 */
+	public int serach1(int[] source){
+		int total = 55;
+		for(int i=0; i<source.length; i++){
+			total -= source[i];
+		}
+		return total;
+	}
+	/**
+	 * 方法一：利用hash，利用这10个数作为键值将所有的值默认为0，遍历数组将存在的数字的值都变成1
+	 * @param source
+	 * @return
+	 */
+	public int serach2(int[] source){
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		for(int i=0; i<10; i++){
+			map.put(i, 0);
+		}
+		for(int i=0; i<source.length; i++){
+			map.put(source[i], 1);
+		}
+		for(int i: map.keySet()){
+			if(map.get(i).equals(0)){
+				return i;
+			}
+		}
+		return 0;
+	}
+}
