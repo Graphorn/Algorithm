2bef01b4d9cf128d89ce4798433b2c43de973aae
seektech@126.com <limiao5002361995>
2016-10-25 21:20:12 +0800

    414 completed

--- src/limiao/leetcode/No_414_ThirdMaximumNumber.java
+++ src/limiao/leetcode/No_414_ThirdMaximumNumber.java
@@ -15,42 +15,38 @@ public class No_414_ThirdMaximumNumber {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int source[] = {2,2,3,1};
+		int source[] = {2,2,3};
 		System.out.println(thirdMax(source));
 	}
 	public static int thirdMax(int[] nums) {
-		if(nums.length == 1) return nums[0];
-		if(nums.length == 2) return nums[0]>=nums[1]?nums[0]:nums[1];
-		
-		List<Integer> list = new ArrayList<>();
-		list.add(nums[0]);
-		for(int i=1; i<nums.length; i++){
-			if(nums[i]<list.get(list.size()-1)){
-				list.add(nums[i]);
-			}
-			if(nums[i]>list.get(list.size()-1)){
-				int temp = list.get(list.size()-1);
-				list.set(list.size()-1,nums[i]);
-				list.add(temp);
-			}
-			if(list.size() == 3) break;
-		}
-		//haha
-		
-		for(int i=0; i<nums.length; i++){
-			if(nums[i]>list.get(2)&&nums[i]<list.get(1)){
-				list.set(2, nums[i]);
-			}
-			if(nums[i]>list.get(1)&&nums[i]<list.get(0)){
-				list.set(2, list.get(1));
-				list.set(1, nums[i]);
-			}
-			if(nums[i]>list.get(0)){
-				list.set(2, list.get(1));
-				list.set(1, list.get(0));
-				list.set(0, nums[i]);
-			}
-		}
-        return nums[2];
+		List<Integer> lst = new ArrayList<>();
+        boolean equal;
+        int tmp;
+
+        for (int i=0; i<nums.length; i++) {
+            equal = false;
+            int j=0;
+            for (;j<3&&j<lst.size(); j++) {
+                tmp = lst.get(j);
+                if (tmp == nums[i]) {
+                    equal = true;
+                    break;
+                }
+                else if (tmp < nums[i]) {
+                    break;
+                }
+            }
+            if (!equal && j<3)
+            lst.add(j, nums[i]);
+        }
+
+        if (lst.size() < 3) {
+            if (lst.size() > 0) {
+                return lst.get(0);
+            }
+            return 0;
+        }
+
+        return lst.get(2);
     }
 }
