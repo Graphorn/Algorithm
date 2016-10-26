0bfa0793d2de2684a15e2ebfe589e5a25685040a
seektech@126.com <limiao5002361995>
2016-10-26 21:30:49 +0800

    array

---
+++ src/limiao/leetcode/No_26_RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,29 @@
+package limiao.leetcode;
+
+/**
+ * 26. Remove Duplicates from Sorted Array
+ * @author seektech
+ * Oct 26, 2016 8:33:48 PM
+ */
+public class No_26_RemoveDuplicatesfromSortedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int removeDuplicates(int[] nums) {
+		if(nums.length < 2) return nums.length;
+		
+		int count = 0;
+        for(int i=0; i<nums.length-1; i++){
+        	if(nums[i] == nums[i+1]) continue;
+        	else{
+        		nums[count] = nums[i];
+        		count++;
+        	} 
+        }
+        // 每一次都是拿当前的和后一个比较，那么最后一个一定是不会重复的，必须保留
+        nums[count] = nums[nums.length-1];
+        return ++count;
+    }
+}
