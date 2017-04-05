d3a7d207b51335ad7a2cb7a0c4496131fd9930d6
seektech <seektech@126.com>
2017-04-05 16:38:22 +0800

    344

---
+++ src/limiao/leetcode/No_125_ValidPalindrome.java
@@ -0,0 +1,37 @@
+package limiao.leetcode;
+
+/**
+ * @author limiao
+ * Apr 1, 2017 5:09:41 PM
+ * 
+ */
+public class No_125_ValidPalindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+	public boolean isPalindrome(String s) {
+		if(s.isEmpty() == true) return true;
+		int head = 0;
+		int tail = s.length()-1;
+		while(head<=tail){
+			char cHead = s.charAt(head);
+			char cTail = s.charAt(tail);
+			if(!Character.isLetterOrDigit(cHead)) head++;
+			else if(!Character.isLetterOrDigit(cTail)) tail--;
+			else{
+				if(Character.toLowerCase(cHead) == Character.toLowerCase(cTail)){
+					head++;
+					tail--;
+				}else{
+					return false;
+				}
+			}
+		}
+		
+		return true;
+    }
+}
---
+++ src/limiao/leetcode/No_344_ReverseString.java
@@ -0,0 +1,26 @@
+package limiao.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author limiao
+ * Mar 31, 2017 10:51:10 AM
+ * 
+ * 344. Reverse String
+ */
+public class No_344_ReverseString {
+
+	public static void main(String[] args) {
+		System.out.println(reverseString("hello"));
+	}
+	public static String reverseString(String s) {
+		char ch[] = s.toCharArray();
+		for(int i=0;i<ch.length/2;i++){
+			char tmp = ch[i];
+			ch[i] = ch[ch.length-1-i];
+			ch[ch.length-1-i] = tmp;		
+		}
+        return String.valueOf(ch);
+    }
+}
